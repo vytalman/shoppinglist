34e1f8f0261308f2336d12c5df8fdf24da9f89ab
vytalman <ryanmesa@gmail.com>
2015-07-20 23:56:37 -0400

    fix sortable list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    console.log("ready!");
+	// Sortable list
+	$('#list').sortable( {
+		axis: "y"
+	});
+
+    // Show delete button when hover over items in list
+    $('#list').on('mouseenter', 'li', function() {
+    	$(this).children('.delete').toggleClass('show');
+    });
+    $('#list').on('mouseleave', 'li', function() {
+    	$(this).children('.delete').toggleClass('show');
+    });
 });

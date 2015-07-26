ce07fba7ff3241ef43f49151ed86b7ed883a4b89
vytalman <ryanmesa@gmail.com>
2015-07-26 01:02:30 -0400

    configure delete button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+	// Add to list
+	
+	
 	// Sortable list
 	$('#list').sortable( {
 		axis: "y"
+	});
+
+	// Checkbox functionality
+	$('.box').on('click', function() {
+		$(this).toggleClass('checked');
 	});
 
     // Show delete button when hover over items in list
@@ -10,5 +18,10 @@ $(document).ready(function() {
     });
     $('#list').on('mouseleave', 'li', function() {
     	$(this).children('.delete').toggleClass('show');
+    });
+
+    // Delete list items
+    $('.delete').on('click', function() {
+    	$(this).parent().remove();
     });
 });

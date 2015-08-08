ae672314f38a208e8a5fbd8acef86014a45a6678
vytalman <ryanmesa@gmail.com>
2015-08-08 03:46:40 -0400

    add button functional

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
-	// Add to list
-	
-	
+	// Add to list using button
+	$('#add-button').click(function() {
+		var inputbox = document.getElementById('input');
+		var inputval = inputbox.value;
+
+		if(!$.trim($('#input').val())) {
+			alert('Please enter text to add to the shopping list');
+		} else {
+			$('<li class="item"></li>').appendTo('#list').html('<div class="box"></div>' + inputval + '<img class="delete" src="images/delete.png">');
+
+		// Clear input box
+		document.getElementById('input').value = '';
+		};
+	});
+
 	// Sortable list
 	$('#list').sortable( {
 		axis: "y"
 	});
 
 	// Checkbox functionality
-	$('.box').on('click', function() {
+	$('#list').on('click', '.box', function() {
 		$(this).toggleClass('checked');
 	});
 
@@ -21,7 +33,7 @@ $(document).ready(function() {
     });
 
     // Delete list items
-    $('.delete').on('click', function() {
+    $('#list').on('click', '.delete', function() {
     	$(this).parent().remove();
     });
 });
